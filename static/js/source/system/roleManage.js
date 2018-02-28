2300f8e874df4dd3de63c08bdfc34eea06a62164
candy <2881516510@qq.com>
2018-02-28 11:02:59 +0800

    系统管理部分页面(角色管理)

--- static/js/source/system/roleManage.js
+++ static/js/source/system/roleManage.js
@@ -1,19 +1,19 @@
 let roleData = [{
-    key: '1',
+    key: 1,
     name: '采购员',
     role: 'APPLY',
     describe: '采购药品',
     lock: false,
     type: '拓展'
 },{
-    key: '2',
+    key: 2,
     name: '配送员',
     role: 'CHECK',
     describe: '配送试剂',
     lock: true,
     type: '拓展'
 }]
-
+let id = 1000
 new Vue({
     el: '#app',
     data() {
@@ -21,27 +21,82 @@ new Vue({
             roleData: roleData,
             roleVisible: false,
             roleForm: {
-                key: '',
+                key: null,
                 name: '',
                 describe: ''
             },
-            roleTitle: '新增角色'
+            role: {
+                title: '新增角色',
+                type: 'add'
+            },
+            configRoleVisible: true,
+            configRole: [{
+                id: 1,
+                label: '全部',
+                children: [{
+                    id: 2,
+                    label: '试剂入库',
+                    children: [{
+                        id: 4,
+                        label: '试剂入库',
+                    }]
+                },{
+                    id: 3,
+                    label: '试剂出库',
+                    children: [{
+                        id: 5,
+                        label: '出库管理',
+                    },{
+                        id: 6,
+                        label: '出库查询',
+                    },{
+                        id: 7,
+                        label: '配送管理',
+                    },{
+                        id: 8,
+                        label: '自由签收',
+                    },{
+                        id: 9,
+                        label: '配货管理',
+                    }]
+                },{
+                    id: 4,
+                    label: '归还管理',
+                    children: [{
+                        id: 4,
+                        label: '试剂入库',
+                    }]
+                }]
+            }]
         }
     },
     methods: {
         lockChange(status){
 
         },
-        submitRole(form) {
+        submitRole(type,form) {
             var that = this;
             this.$refs[form].validate(function(valid) {
                 if (valid) {
-                    roleData.forEach(item => {
-                        if(item.key === that[form].key) {
-                            item.name = that[form].name;
-                            item.describe = that[form].describe;
-                        }
-                    })
+                    if(type === 'edit') {
+                        roleData.forEach(item => {
+                            if(item.key === that[form].key) {
+                                item.name = that[form].name;
+                                item.describe = that[form].describe;
+                            }
+                        })
+                    } else {
+
+                        roleData.push({
+                            key: roleData.length+1,
+                            name: that[form].name,
+                            role: 'APPLY',
+                            describe: that[form].describe,
+                            lock: false,
+                            type: '拓展'
+                        });
+                    }
+
                     console.log('success')
                 } else {
                     console.log('fail')
@@ -49,16 +104,17 @@ new Vue({
             });
             this.roleVisible = false;
         },
-        role(type,row) {
+        roleClick(type,row) {
             if(type === 'add') {
-                this.roleTitle = '新增角色'
+                this.role.title = '新增角色';
             } else {
                 this.roleForm.name = row.name;
                 this.roleForm.describe = row.describe;
                 this.roleForm.key = row.key;
-                this.roleTitle = '编辑角色'
+                this.role.title = '编辑角色'
             }
             this.roleVisible = true;
+            this.role.type = type
             console.log(row)
         }
     }
